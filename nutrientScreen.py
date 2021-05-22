9fb02affc520395284b24e5d18801b0ee8f836a0
Simeon <gar17052@byui.edu>
2021-05-21 18:39:26 -0600

    Got the slider and buttons to work properly together and added variables from main

--- nutrientScreen.py
+++ nutrientScreen.py
@@ -1,23 +1,43 @@
+from mistingScreen import MININTERVAL
+from lightingScreen import BUTTONSTYLE
 import sys  # to get the screen size and close the window
-from PyQt5.QtCore import Qt     # all the pyqt5 libraries we need
+from PyQt5.QtCore import QSize, Qt, qSetFieldWidth     # all the pyqt5 libraries we need
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from OpButton import OpButton
 
-BUTTONFONT = QFont("times", 30) # font for the tabs
-LABELFONT1 = QFont("times", 60) # font for the bigger labels (reservoir, temperature)
-LABELFONT2 = QFont("times", 30) # font for the readings for the labels (60%)
-WB = 500 # Width buffer for the tabs
-HB = -65 # Hight buffer for the tabs
+SIZE = (480, 800)
+
+FONT = "Cambria"
+
+BUTTONFONT = QFont(FONT, (int(SIZE[0]/12))) # font for the tabs
+LABELFONT1 = QFont(FONT, (int(SIZE[0]/16))) # font for the bigger labels (reservoir, temperature)
+LABELFONT2 = QFont(FONT, (int(SIZE[0]/19))) # font for the readings for the labels (60%)
+# WB = 500 # Width buffer for the tabs
+# HB = -65 # Hight buffer for the tabs
+BUTTONSTYLE = "font: bold; background-color: white; border: none; border-radius: 40px; color: rgb(107,138,116)" # rgb(93,173,236)
 MINNUTR = 0
 MAXNUTR = 1500
 INTERVAL = 25
+
+def setButton(button, text, func):
+    button.setStyleSheet(BUTTONSTYLE)
+    button.setFont(BUTTONFONT)
+    button.setText(text)
+    button.clicked.connect(func)
+    button.setFixedSize(100, 100)                                                     # this will set the size of the button     
+    button.animation.stop()
+    button.animation.start()
+    button.animation.setDuration(250)
 
 class nutrientScreen(QWidget):
     def __init__(self, parent):
         super(nutrientScreen, self).__init__(parent)
         # self.BWIDTH = BWIDTH
         # self.BHEIGHT = BHIGHT
-        self.nutrAmount = 300
+        # self.lastValue = 0
+        self.par = parent
+        self.nutrAmount = parent.NutrAmt
         self.initUI(parent)
 
     def initUI(self, parent): # initUI function will set up all the labels and buttons we want for the ui                     
@@ -25,19 +45,52 @@ class nutrientScreen(QWidget):
         self.Nutr = QLabel(self)              # Make the Reservoir label                             
         self.Nutr.setText("Nutrients:")        # Set the text                                         
         self.Nutr.setFont(LABELFONT1)         # Set the font for the label                           
-        self.Nutr.setAlignment(Qt.AlignCenter)# This will align it to the center                             
+        self.Nutr.setAlignment(Qt.AlignCenter)# This will align it to the center
+        self.Nutr.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.Nutr.setStyleSheet("font: bold; color: white")                    
 
         self.amount = QLabel(self)
         self.amount.setText(str(self.nutrAmount) + " ppm")
         self.amount.setFont(LABELFONT2)
-        self.amount.setAlignment(Qt.AlignCenter)             
+        self.amount.setAlignment(Qt.AlignCenter)
+        self.amount.setStyleSheet("font: bold; color: white")
+
+        self.increment = OpButton()
+        setButton(self.increment, "+", self.inc)
+
+        self.decrement = OpButton()
+        setButton(self.decrement, "-", self.dec)
+
+        self.slider = QSlider(orientation=Qt.Horizontal)
+        self.slider.setFixedWidth((int(SIZE[1]/2)))
+        self.slider.setFixedHeight(100)
+        self.slider.setStyleSheet("""QSlider::groove:horizontal {
+                                            border: 0px solid #bbb;
+                                            background: white;
+                                            height: 30px;
+                                            border-radius: 15px;
+                                        }
+                                            QSlider::handle:horizontal {
+                                            background: rgb(107,138,116);
+                                            border: 5px solid #fff;
+                                            width: 70px;
+                                            margin-top: -20px;
+                                            margin-bottom: -20px;
+                                            border-radius: 35px;
+                                        }
+                                    """)
+        
+        self.slider.setMinimum(MINNUTR)
+        self.slider.setMaximum(MAXNUTR/INTERVAL)
+        self.slider.setValue(int(self.nutrAmount / (int(INTERVAL))))
+        self.slider.valueChanged.connect(self.slide)
+    
 
         tabs = QVBoxLayout()    # Make the layout for the tabs
         labels = QVBoxLayout()  # Make the layout for the labels, this will hold the layouts for each set of labels as it will better space them
         nutrLay = QVBoxLayout()  # Make the layout for the Reservoir
-        # temLab = QVBoxLayout()  # Make the layout for the Temperature
-        # humLab = QVBoxLayout()  # Make the layout for the Humidity
-        # presLab = QVBoxLayout() # Make the layout for the Pressure
+        nutrBtn = QHBoxLayout()  # Make the layout for the Temperature
+        slideLay = QHBoxLayout()
         
         columns = QHBoxLayout() # This will be the main layout
         columns.setContentsMargins(0,0,0,0)  # This will remove the margin around the layouts, making the tabs reach the edge of the screen.
@@ -47,21 +100,16 @@ class nutrientScreen(QWidget):
 
         nutrLay.addWidget(self.Nutr) # Add the reservoir label to the reservoir layout  
         nutrLay.addWidget(self.amount)  # Add the reservoir amount to the reservoir layout
+        
+        nutrBtn.addWidget(self.decrement)
+        nutrBtn.addWidget(self.increment)
 
-        # temLab.addWidget(self.Temperature) # Add the temperature label to the temperature layout
-        # temLab.addWidget(self.T_amount)    # Add the temperature amount to the temperature layout
-
-        # humLab.addWidget(self.Humidity) # Add the humidity label to the humidity layout
-        # humLab.addWidget(self.H_amount) # Add the humidity amount to the humidity layout
-
-        # presLab.addWidget(self.Pressure) # Add the pressure label to the pressure layout
-        # presLab.addWidget(self.P_amount) # Add the pressure amount to the pressure layout
+        slideLay.addWidget(self.slider)
 
         # Add all the label layouts to the main label layouts, this is done so they are grouped together nicely and easier to manage
         labels.addItem(nutrLay) 
-        # labels.addItem(temLab) 
-        # labels.addItem(humLab) 
-        # labels.addItem(presLab)
+        labels.addItem(nutrBtn)
+        labels.addItem(slideLay)
         
         # Add the tabs to the tabs layout
         tabs.addWidget(parent.Home)
@@ -78,3 +126,29 @@ class nutrientScreen(QWidget):
 
         # make the screen maximized when it starts
         self.showMaximized()
+
+    def inc(self):
+        self.increment.animation.stop()
+        self.increment.animation.start()
+        if (self.nutrAmount < MAXNUTR):
+            self.nutrAmount = self.nutrAmount + INTERVAL
+            self.par.NutrAmt = self.nutrAmount
+        self.slider.setValue(int(self.nutrAmount / (int(INTERVAL))))
+
+        self.amount.setText(str(self.nutrAmount) + " ppm")
+
+    def dec(self):
+        self.decrement.animation.stop()
+        self.decrement.animation.start()
+        if (self.nutrAmount > MINNUTR):
+            self.nutrAmount = self.nutrAmount - INTERVAL
+            self.par.NutrAmt = self.nutrAmount
+        self.slider.setValue(int(self.nutrAmount / (int(INTERVAL))))
+
+        self.amount.setText(str(self.nutrAmount) + " ppm")
+    
+    def slide(self, value):
+        
+        self.nutrAmount = value * INTERVAL
+        self.par.NutrAmt = self.nutrAmount
+        self.amount.setText(str(self.nutrAmount) + " ppm")
